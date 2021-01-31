2f853e537c88dbe1027f92af2cf7e4d06166d7a2
mohammed <meddce1997@gmail.com>
2021-01-31 18:30:58 +0300

    third commit

--- app/src/main/java/com/yemensoft/autoupdate/MainActivity.java
+++ app/src/main/java/com/yemensoft/autoupdate/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.javiersantos.appupdater.AppUpdater;
 import com.github.javiersantos.appupdater.enums.Display;
@@ -28,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity2.class));
     }
 
+    public void GoSecActivity2(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
+        Toast.makeText(this, "gooooooo", Toast.LENGTH_SHORT).show();
+
+    }
 }
